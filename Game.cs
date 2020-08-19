f00a1ce3ba780b44c50fff209d91f0565d9d1d1e
Cailey Bianchini <cailey.m.bianchini@mail.com>
2020-08-19 14:08:22 -0500

    if roles

--- Game.cs
+++ Game.cs
@@ -46,8 +46,8 @@ namespace HelloWorld
             
             //maxlevelreached = level == maxlevel;
 
-            //menu
-            bool ready = true;
+                //menu
+            //bool ready = true;
 
             //start
             Console.WriteLine("What is your name?");
@@ -58,19 +58,44 @@ namespace HelloWorld
             Console.WriteLine("The world is a violent one. Many creatures live in this world: Humans, Elves, Drawfs, Spirits, and much more. This world use to be quite a peacefull one until the fowl Demons showed up. At first it was random killings, lone merchants traveling in between towns, farmers that where out on their feild.");
             Console.WriteLine("Everything changed when the Demon King appeared. They're random attacks became organized, the damage done tripled, and the causualties worsened. We were losing hope. That was until a profacy was shared, 'Summon a Hero and the world shall be saved.' and thus a slimmer of hope was born!");
             Console.WriteLine("~~~~~~~~~~");
-
+             
             //start
-            Console.WriteLine("You awoke in a bright marble room, standing in front of you a tall and goddess like dark elf. There was a halo behing her making her even more goddess like, she gives you small smile and open her arms.");
+            Console.WriteLine("You awoke in a bright marble room, standing in front of you a tall and goddess like dark elfand her eyes had galxies in them. There was a halo behing her making her even more goddess like, she gives you small smile and open her arms.");
             Console.WriteLine("'You were brought to our world to save us Hero " + name + "!'");
             Console.WriteLine("What type of Hero Class are you: ");
             string role = Console.ReadLine();
-            Console.WriteLine("     ");
+            Console.WriteLine("I'm sorry but I can't tell, what race are you?");
+            string race = Console.ReadLine();
+           Console.WriteLine("  ");
             Console.WriteLine("-Level Gained-");
             level = level+1;
             Console.WriteLine("     ");
             //telling the users stat
+
+            if (role == "mage")
+            {
+                PlayerHealth = PlayerHealth - 50;
+                mana = mana + 30;
+                playersattack = playersattack - 10;
+
+
+            }
+            if (role == "knight")
+            {
+
+            }
+            if (role == "necromancer")
+            {
+
+            }
+            if (role == "bard")
+            {
+
+            }
+
             Console.WriteLine("Current level: " + level);
             Console.WriteLine("Hero role: " + role);
+            Console.WriteLine("Race : " + race);
             Console.WriteLine("Health: " + PlayerHealth);
             Console.WriteLine("Mana: " + mana);
             Console.WriteLine("Attack: " + playersattack);
